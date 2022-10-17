2af648af2fa16d37d5ebad9f70450d70ee2dd42b
Jesse Pararadero <72135199+jesse345@users.noreply.github.com>
2022-10-17 16:02:10 +0800

    test

--- profile_app/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from profile_app.models import MProfile
-
-class ProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MProfile
-        fields = '__all__' 
-
